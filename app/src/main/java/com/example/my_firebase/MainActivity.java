219e8e4a0d00f39adb86231058260e245a167160
mustafa mohammmed <mustafaastal99@gmail.com>
2020-09-17 14:20:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/my_firebase/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.my_firebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et_name;
+    EditText et_pass;
+    Button btn_save;
+    private String REF_NAME="name";
+    private String REF_pass="password";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_name=findViewById(R.id.et_name);
+        et_pass=findViewById(R.id.et_pass);
+        btn_save=findViewById(R.id.btn_save);
+
+
+        // Write a message to the database
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference reference = database.getReference(REF_NAME);
+
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                reference.push().setValue(et_name.getText().toString());
+
+            }
+        });
+
+        // Read from the database
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.child(REF_NAME).getValue(String.class);
+                 Log.d("TAG", "******************************************************Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+}
